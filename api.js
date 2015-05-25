fd1459647dea9d75929f4743a66bef0f8e0e1e1f
Manish <manish.garg@paxcel.net>
2015-05-25 18:02:57 +0530

    initial commit

---
+++ api.js
@@ -0,0 +1,59 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "add",
+        "base",
+        "file",
+        "git",
+        "prompt",
+        "remove",
+        "update"
+    ],
+    "modules": [
+        "add",
+        "base",
+        "file",
+        "git",
+        "prompt",
+        "remove",
+        "update"
+    ],
+    "allModules": [
+        {
+            "displayName": "add",
+            "name": "add",
+            "description": "Various functions to add new post, page, layout, section or team member."
+        },
+        {
+            "displayName": "base",
+            "name": "base",
+            "description": "Provides the base object"
+        },
+        {
+            "displayName": "file",
+            "name": "file",
+            "description": "Various functions related to file handling."
+        },
+        {
+            "displayName": "git",
+            "name": "git",
+            "description": "Various functions related to git and github."
+        },
+        {
+            "displayName": "prompt",
+            "name": "prompt",
+            "description": "Various functions related to prompting."
+        },
+        {
+            "displayName": "remove",
+            "name": "remove",
+            "description": "Various functions to remove post, page, layout, section or team member."
+        },
+        {
+            "displayName": "update",
+            "name": "update",
+            "description": "Various functions to update team member."
+        }
+    ]
+} };
+});
